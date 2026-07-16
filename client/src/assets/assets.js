bc573f680831dd3ec26f561b9490d24b9a4a1fac
Abdullah Jutt <165588844+Abdullah-Hunjra1@users.noreply.github.com>
2026-07-17 01:14:01 +0500

    Refactor assets.js to comment out unused imports
    Comment out unused image imports and assets.

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -2,9 +2,9 @@
 import profile_pic from './profile_pic.png'
 import abd from './abd.jpg'
 import contact_image from './contact_image.png'
-import about_image from './about_image.png'
-import logo from './logo.svg'
-import dropdown_icon from './dropdown_icon.svg'
+// import about_image from './about_image.png'
+// import logo from './logo.svg'
+// import dropdown_icon from './dropdown_icon.svg'
 import cross_icon from './cross_icon.png'
 import upload_icon from './upload_icon.png'
 import person from './person.jpg'
@@ -15,13 +15,13 @@ import hero from './hero.jpg'
 
 export const assets = {
     // header_img,
-    logo,
+    // logo,
     profile_pic,
     abd,
     contact_image,
-    about_image,
+    // about_image,
     cross_icon,
-    dropdown_icon,
+    // dropdown_icon,
     upload_icon,
     person,
     selfcare,
