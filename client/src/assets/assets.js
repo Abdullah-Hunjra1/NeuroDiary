fd2636cf55afbed5225c2d926377a6d7ff73b7a0
Abdullah-Hunjra1 <abdullahhunjra111@gmail.com>
2026-07-26 02:39:57 +0500

    assets.js update

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,10 +1,6 @@
-// import header_img from './header_img.png'
 import profile_pic from './profile_pic.png'
 import abd from './abd.jpg'
 import contact_image from './contact_image.png'
-// import about_image from './about_image.png'
-// import logo from './logo.svg'
-// import dropdown_icon from './dropdown_icon.svg'
 import cross_icon from './cross_icon.png'
 import upload_icon from './upload_icon.png'
 import person from './person.jpg'
@@ -14,14 +10,10 @@ import hero from './hero.jpg'
 
 
 export const assets = {
-    // header_img,
-    // logo,
     profile_pic,
     abd,
     contact_image,
-    // about_image,
     cross_icon,
-    // dropdown_icon,
     upload_icon,
     person,
     selfcare,
